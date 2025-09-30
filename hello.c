1883be328559938ab0ccfb930530b652a0708e36
Dunant <ngouadjeudunant@gmail.com>
2025-09-30 23:40:35 +0200

    J'ai ajouter le fichier hello.c

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   // bibliothèque standard pour printf
+
+int main() {
+    printf("Hello, world!\n");   // affiche du texte à l'écran
+    return 0;   // programme terminé avec succès
+}
