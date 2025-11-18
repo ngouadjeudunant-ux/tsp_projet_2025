a1c61f4c206346a10e362d9b3a2840dc72ab33f0
RusselGihalu <gihalurussel@gmail.com>
2025-11-18 14:16:32 +0100

    Version corrigée de Rw

--- include/algo_rw.h
+++ include/algo_rw.h
@@ -4,6 +4,5 @@
 #include "tsp_types.h"
 
 int* rw_tour(const TSP_Instance *inst);
-void int_array_pop(int ** array, int index, int length);
 
 #endif
--- src/algo_rw.c
+++ src/algo_rw.c
@@ -9,28 +9,36 @@
 #include "algo_rw.h"
 #include "tsp_parser.h"
 
-void int_array_pop(int ** array, int index, int length){
-    for (int i = index; i < length-1; i++){
-        (*array)[i] = (*array)[i+1];
-    }
-}
+ #include <stdlib.h>
+#include <time.h>
+#include "algo_rw.h"
 
 int* rw_tour(const TSP_Instance *inst) {
     srand(time(NULL));
-    int n = inst->dimension;
-    int pick;
-    if (n <= 0 || !inst->dist) return NULL;
-    
-    int *tour = malloc(n * sizeof(int));
-    int *visite = calloc(n, sizeof(int));
-    if (!tour || !visite) return NULL;
-    for (int i = 0; i < n; i++) visite[i] = i;
 
-    for (int i = 0; i < n; i++){
-        pick = (rand()%(n-i));
+    int n = inst->dimension;
+    if (n <= 0 || !inst->dist) return NULL;
+
+    int *tour = malloc((n + 1) * sizeof(int));  // alloue une case de plus
+    int *visite = malloc(n * sizeof(int));
+    if (!tour || !visite) return NULL;
+
+    for (int i = 0; i < n; i++)
+        visite[i] = i;
+
+    int remaining = n;
+
+    for (int i = 0; i < n; i++) {
+        int pick = rand() % remaining;
         tour[i] = visite[pick];
-        int_array_pop(&visite, pick, n-i);
+
+        for (int j = pick; j < remaining - 1; j++)
+            visite[j] = visite[j + 1];
+
+        remaining--;
     }
+
+    tour[n] = tour[0];  // on boucle le tour (retour à la ville de départ)
     free(visite);
     return tour;
 }
